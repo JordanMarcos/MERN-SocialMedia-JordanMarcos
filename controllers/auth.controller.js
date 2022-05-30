602fba6bb3c2e6cdb3689174ea815878362aab14
JordanMarcos <jordan.marcos.oclock@gmail.com>
2022-05-30 17:53:38 +0200

    Auth controller signIn done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,21 @@
 // Ce controller fait l'inscription, la connexion et la déconnexion de l'user
+
 // Je prends l'user model
 const UserModel = require('../models/user.model');
+
+// Je prends JWT
+const jwt = require('jsonwebtoken');
+
+// Je donne une durée de vie maximale de 3 jours au cookie
+const maxAge = 3 * 24 * 60 * 60 * 1000
+
+// Fonction qui créer les tokens
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.TOKEN_SECRET, {
+        expiresIn: maxAge
+    })
+};
+
 
 // Je créer et export le controller signUp
 module.exports.signUp = async (req, res) => {
@@ -12,6 +27,24 @@ module.exports.signUp = async (req, res) => {
         res.status(201).json({ user: user._id});
     }
     catch(err) {
-        res.status(200).send({ err })
+        res.status(200).send({ err });
     }
+};
+
+// Je créer et export le controller signIn
+module.exports.signIn = async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await UserModel.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge });
+        res.status(200).json({user: user._id});
+    } catch (err) {
+        res.status(200).json(err);
+    }
+}
+
+module.exports.logout = (req,res) => {
+
 }
--- models/user.model.js
+++ models/user.model.js
@@ -60,7 +60,20 @@ userSchema.pre("save", async function(next) {
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
-})
+});
+
+// Fonction permet de comparer les mdp cryptés
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth){
+            return user
+        }
+        throw Error('Mot de passe incorrecte')
+    }
+    throw Error('Mot de passe incorrecte')
+}
 
 const UserModel = mongoose.model('user', userSchema);
 
