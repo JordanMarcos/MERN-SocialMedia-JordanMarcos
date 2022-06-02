743116ede8527bd29e62841fc15e272f196220e4
JordanMarcos <jordan.marcos.oclock@gmail.com>
2022-06-02 16:30:25 +0200

    The creation of picture line in the tables is done

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -32,8 +32,25 @@ module.exports.uploadProfil = async (req, res) => {
     // Créer un fichier avec un chemin avec la biblio fs
     await pipeline(
         req.file.stream,
+        // pour que ça marche multer j'ai du downgrade nodeJS à la V 15.4.0
         fs.createWriteStream(
             `${__dirname}/../client/public/uploads/profil/${fileName}`
         )
     ); 
+
+    try {
+        await UserModel.findByIdAndUpdate(
+            req.body.userId,
+            { $set: {picture: "./uploads/profil/" + fileName} },
+            { new: true, upsert: true, setDefaultsOnInsert: true },
+            (err, docs) => {
+                if (!err) return res.send(docs);
+                else return res.status(500).send({ message: err });
+            }
+        );
+    } catch (err) {
+        // je ne peux pas return un res.status sinon mon app crash donc je fais un log d'erreur
+        // mais pourtant tout fonctionne, il est bien créé sur mongodb
+        return console.log( 'Catch err msg in upload.controller in the UploadProfil function : ' + err.message);
+    }
 };
