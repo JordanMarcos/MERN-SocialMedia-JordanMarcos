8290c5421eea95971de0d4c7c1bf1a931fc82afa
JordanMarcos <jordan.marcos.oclock@gmail.com>
2022-06-03 13:56:58 +0200

    CORS done, backend fully finished

--- server.js
+++ server.js
@@ -1,50 +1,58 @@
-// Je prends express 
-const express = require('express');
-
+// Je prends express
+const express = require("express");
 // Je prends body-parser
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
 // Je prends cookie-parser pour pouvoir lire les cookie avec nodeJS
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 // Je prends les routes
-const userRoutes = require('./routes/user.routes');
-const postRoutes = require('./routes/post.routes');
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
 
 // Je donne le chemin à dotenv pour accéder aux variables d'environements
-require('dotenv').config({path: './config/.env'});
-
+require("dotenv").config({ path: "./config/.env" });
 // Je prends db.js pour accéder à la BDD
-require('./config/db');
+require("./config/db");
 
 // Je prends les middlewares checkUser et requireAuth
-const {checkUser, requireAuth} = require('./middleware/auth.middleware');
-
-// 'App' devient express() 
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+// Je prends cors pour gérer les autorisations des fetch
+const cors = require("cors");
+// 'App' devient express()
 const app = express();
+
+// Variable qui contient les autorisations fetch
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentals: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+
+// J'autorise les clients du site a faire des requêtes
+app.use(cors(corsOptions));
 
 // J'utiliser bodyParser pour pouvoir traiter les requetes du body (req.body)
 app.use(bodyParser.json());
-
 // J'utiliser bodyParser pour pouvoir lire les req.params (url)
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 // J'utilise cookieParser afin de pouvoir lire les cookies avec nodeJS
 app.use(cookieParser());
 
-// Middleware pour vérifier le token jwt de l'user sur toutes les requetes 
-app.get('*', checkUser);
-
+// Middleware pour vérifier le token jwt de l'user sur toutes les requetes
+app.get("*", checkUser);
 //Middleware pour vérifier la premiere connexion de l'user
-app.get('/jwtid', requireAuth, (req, res) => {
-    res.status(200).send(res.locals.user._id);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
 });
 
 // Routes
-app.use('/api/user', userRoutes);
-app.use('/api/post',postRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 // Server : je demande à Express d'écouter sur le port déclaré dans les var d'environements
 app.listen(process.env.PORT, () => {
-    console.log(`J'écoute sur le port ${process.env.PORT} `);
-})
+  console.log(`J'écoute sur le port ${process.env.PORT} `);
+});
